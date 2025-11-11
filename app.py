73ef0bcf2b31b3b332193ae3196ba2d2a01e0ef0
Alba Vera <veracaballeroalba@gmail.com>
2025-11-11 18:11:20 +0100

    Debugged problem with showing history in index

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def index():
         return redirect("/login")
 
     # TO DO: Retrieve and display user's writing history
-    writing_history = db.execute("SELECT * FROM writing_history WHERE user_id = ?", (session["user_id"],)).fetchall()
-    return render_template("index.html", history=writing_history)
+    writing_history = db.execute("SELECT * FROM writing_history WHERE id_user = ?", (session["user_id"],)).fetchall()
+    return render_template("index.html", docs=writing_history)
 
 @app.route("/vocabulary", methods=["GET", "POST"])
 def vocabulary():
